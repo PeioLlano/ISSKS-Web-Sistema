70f454efb295e7e0826ae69a9a4db7f2a25ae102
PeioLlano <peiollano@gmail.com>
2021-09-30 17:21:00 +0200

    Js pixka bat

--- app/js/erregistratu.js
+++ app/js/erregistratu.js
@@ -1,35 +1,56 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("form").addEventListener('submit', datuakKonprobatu); 
-  });
-  
-  function datuakKonprobatu(evento) {
-    evento.preventDefault();
-    var izena = document.getElementsByName('izena').value;
-    if(izena.length != 0) {
-      alert('Ez duzu izenik sartu');
-      return;
-    }
-    var NAN = document.getElementsByName('NAN').value;
-    var formatua;
-    formatua = /^\d{8}[a-zA-Z]$/;
+ function datuakKonprobatu() {
+  if(!izenaKonprobatu(document.getElementsByName('izena').value)) {
+    return
+  }
+  if(!nanKonprobatu(document.getElementsByName('NAN').value)){
+    return;
+  }
+  if(!tlfKonprobatu(document.getElementsByName('tlf').value))
+  this.submit();
+}
 
-    if (formatua.test(NAN)) {
-      zenb = NAN.substr(0,NAN.length-1);
-      letr = NAN.substr(NAN.length-1,1);
-      zenb= zenb % 23;
-      letra='TRWAGMYFPDXBNJZSQVHLCKET'
-      letra=letra.substring(zenb,zenb+1)
-      if(letra!=letr.toUpperCase()){
-        alert('SArtutako NAN-a ez da existitzen.');
-      return;
-      }
-      else{
 
-      }
+function izenaKonprobatu(pIzena) {
+  if(izena.length == 0) {
+    alert('Ez duzu izenik sartu');
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+
+function nanKonprobatu(pNAN) {
+  var formatua;
+  formatua = /^\d{8}[a-zA-Z]$/;
+
+  if (formatua.test(pNAN)) {
+    zenb = pNAN.substr(0,pNAN.length-1);
+    letr = pNAN.substr(pNAN.length-1,1);
+    zenb= zenb % 23;
+    letra='TRWAGMYFPDXBNJZSQVHLCKET'
+    letra=letra.substring(zenb,zenb+1)
+    if(letra!=letr.toUpperCase()){
+      alert('Sartutako NAN-a ez da existitzen.');
+      return false;
     }
     else{
-      alert('NAN formatu okerra sartu duzu.');
-      return;
+      return true;
     }
-    this.submit();
   }
+  else{
+    alert('NAN formatu okerra sartu duzu.');
+    return false;
+  }
+}
+
+function tlfKonprobatu(pTlf) {
+  if(pTlf.length == 9 && pTlf.isInteger()) {
+    return true;
+  }
+  else{
+    alert('Telefono okerra sartu duzu.');
+    return false;
+  }
+}
+  
