807239833cb6db072304498b9ee34ed82b3d7cba
PeioLlano <peiollano@gmail.com>
2021-10-02 11:51:50 +0200

    Erregistrutu javascrit eginda, baina ez du onclick-a egiten

--- app/js/erregistratu.js
+++ app/js/erregistratu.js
@@ -1,11 +1,24 @@
  function datuakKonprobatu() {
   if(!izenaKonprobatu(document.getElementsByName('izena').value)) {
-    return
+    return;
   }
   if(!nanKonprobatu(document.getElementsByName('NAN').value)){
     return;
   }
-  if(!tlfKonprobatu(document.getElementsByName('tlf').value))
+  if(!tlfKonprobatu(document.getElementsByName('tlf').value)){
+    return;
+  }
+  if(!dataKonprobatu(document.getElementsByName('jaiotze').value)){
+    return;
+  }
+  if(!tlfKonprobatu(document.getElementsByName('email').value)){
+    return;
+  }
+  if(document.getElementsByName('pasahitza')==document.getElementsByName('repPasahitza')){
+    alert('Sartutako pasahitzak ez dira berdinak.');
+    return;
+  }
+  alert('Datu guztiak zuzen');
   this.submit();
 }
 
@@ -44,13 +57,34 @@ function nanKonprobatu(pNAN) {
   }
 }
 
+
+function dataKonprobatu(pData) {
+  var dataFormatua = /^\d{2,4}\-\d{1,2}\-\d{1,2}$/;
+  if (dataFormatua.test(pData)) {
+      return true;
+  }
+  else{
+    alert("Data formatu okerra sartu duzu.");
+    return false;
+  }
+}
+
 function tlfKonprobatu(pTlf) {
   if(pTlf.length == 9 && pTlf.isInteger()) {
     return true;
   }
   else{
-    alert('Telefono okerra sartu duzu.');
+    alert('Telefono zenbaki okerra sartu duzu.');
     return false;
+  }
+}
+
+function emailaKonprobatu(pEmail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(pEmail)){
+   return true;
+  } else {
+   alert("Email helbide okerra sartu duzu.");
+   return false;
   }
 }
   
