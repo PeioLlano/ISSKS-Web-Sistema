e77f6f9c149cf7451c75820e3273d1c071e254da
955750 <72309913+955750@users.noreply.github.com>
2021-10-10 12:34:31 +0200

    erregistratu formularioaren arazoa konponduta (1)

--- app/js/erregistratu.js
+++ app/js/erregistratu.js
@@ -1,100 +1,94 @@
- function datuakKonprobatu() {
-  if(!izenaKonprobatu(document.getElementById('izena').value)) {
-    return;
-  }
-  if(!nanKonprobatu(document.getElementById('nan').value)){
-    return;
-  }
-  if(!tlfKonprobatu(document.getElementById('tlf').value)){
-    return;
-  }
-  if(!dataKonprobatu(document.getElementById('jaiotze').value)){
-    return;
-  }
-  if(!emailaKonprobatu(document.getElementById('email').value)){
-    return;
-  }
-  if(document.getElementById('pasahitza').value != document.getElementById('repPasahitza').value){
-    alert('Sartutako pasahitzak ez dira berdinak.');
-    return;
-  }
-  document.getElementById('form').submit();
-}
+ function datuakKonprobatu(evt) {
+     evt.preventDefault()
+     if (!izenaKonprobatu(document.getElementById('izena').value)) {
+         return;
+     }
+     if (!nanKonprobatu(document.getElementById('nan').value)) {
+         return;
+     }
+     if (!tlfKonprobatu(document.getElementById('tlf').value)) {
+         return;
+     }
+     if (!dataKonprobatu(document.getElementById('jaiotze').value)) {
+         return;
+     }
+     if (!emailaKonprobatu(document.getElementById('email').value)) {
+         return;
+     }
+     if (document.getElementById('pasahitza').value != document.getElementById('repPasahitza').value) {
+         alert('Sartutako pasahitzak ez dira berdinak.');
+         return;
+     }
+     document.getElementById('form').submit();
+ }
 
 
-function izenaKonprobatu(pIzena) {
-  if(pIzena.length == 0) {
-    alert('Ez duzu izenik sartu');
-    return false;
-  }
-  else{
-    return true;
-  }
-}
+ function izenaKonprobatu(pIzena) {
+     if (pIzena.length == 0) {
+         alert('Ez duzu izenik sartu');
+         return false;
+     } else {
+         return true;
+     }
+ }
 
-function nanKonprobatu(pNAN) {
-  var formatua;
-  formatua = /^\d{8}[a-zA-Z]$/;
+ function nanKonprobatu(pNAN) {
+     var formatua;
+     formatua = /^\d{8}[a-zA-Z]$/;
 
-  if (formatua.test(pNAN)) {
-    zenb = pNAN.substr(0,pNAN.length-1);
-    letr = pNAN.substr(pNAN.length-1,1);
-    zenb= zenb % 23;
-    letra='TRWAGMYFPDXBNJZSQVHLCKET'
-    letra=letra.substring(zenb,zenb+1)
-    if(letra!=letr.toUpperCase()){
-      alert('Sartutako NAN-a ez da existitzen.');
-      return false;
-    }
-    else{
-      return true;
-    }
-  }
-  else{
-    alert('NAN formatu okerra sartu duzu.');
-    return false;
-  }
-}
+     if (formatua.test(pNAN)) {
+         zenb = pNAN.substr(0, pNAN.length - 1);
+         letr = pNAN.substr(pNAN.length - 1, 1);
+         zenb = zenb % 23;
+         letra = 'TRWAGMYFPDXBNJZSQVHLCKET'
+         letra = letra.substring(zenb, zenb + 1)
+         if (letra != letr.toUpperCase()) {
+             alert('Sartutako NAN-a ez da existitzen.');
+             return false;
+         } else {
+             return true;
+         }
+     } else {
+         alert('NAN formatu okerra sartu duzu.');
+         return false;
+     }
+ }
 
 
-function dataKonprobatu(pData) {
-  var dataFormatua = /^\d{2,4}\-\d{1,2}\-\d{1,2}$/;
-  if (dataFormatua.test(pData)) {
-      return true;
-  }
-  else{
-    alert("Data formatu okerra sartu duzu.");
-    return false;
-  }
-}
+ function dataKonprobatu(pData) {
+     var dataFormatua = /^\d{2,4}\-\d{1,2}\-\d{1,2}$/;
+     if (dataFormatua.test(pData)) {
+         return true;
+     } else {
+         alert("Data formatu okerra sartu duzu.");
+         return false;
+     }
+ }
 
-function tlfKonprobatu(pTlf) {
-  var formatua;
-  formatua = /^\d{9}$/;
+ function tlfKonprobatu(pTlf) {
+     var formatua;
+     formatua = /^\d{9}$/;
 
-  if (formatua.test(pTlf)) {
-    return true;
-  }
-  else{
-    alert('Telefono zenbaki okerra sartu duzu.');
-    return false;
-  }
-}
+     if (formatua.test(pTlf)) {
+         return true;
+     } else {
+         alert('Telefono zenbaki okerra sartu duzu.');
+         return false;
+     }
+ }
 
-function emailaKonprobatu(pEmail) {
-  var formatua;
-  formatua = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
-  if (formatua.test(pEmail)){
-   return true;
-  } else {
-   alert("Email helbide okerra sartu duzu.");
-   return false;
-  }
-}
+ function emailaKonprobatu(pEmail) {
+     var formatua;
+     formatua = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+     if (formatua.test(pEmail)) {
+         return true;
+     } else {
+         alert("Email helbide okerra sartu duzu.");
+         return false;
+     }
+ }
 
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
-}
-
-  
+ function myFunction() {
+     document.getElementById("demo").innerHTML = "Hello World";
+ }
