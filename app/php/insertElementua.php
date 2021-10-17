6b42017c4aba64e2d825fb09ed68dedcb36f9edc
PeioLlano <peiollano@gmail.com>
2021-10-17 13:34:30 +0200

    Insert Elementua

--- app/php/insertElementua.php
+++ app/php/insertElementua.php
@@ -17,18 +17,19 @@
   $gela = "P7I5";
   
   //Kirol eta egun horretan ordu hori hartuta dagoen begiratzeko
-  $ordutegiKonprobaketa = mysqli_query($conn, "SELECT COUNT(*) FROM `elementua` WHERE `kirola`=$kirola AND `data`=$data AND `ordutegia`=$ordutegia; ");
+  $ordutegiKonprobaketa = mysqli_query($conn, "SELECT * FROM `elementua` WHERE `kirola`=$kirola AND `data`=$data AND `ordutegia`=$ordutegia; ");
   if(empty($ordutegiKonprobaketa)){
     //5 elementu dituen begiratzeko
     $elemKonprobaketa = mysqli_query($conn, "SELECT COUNT(*) FROM `elementua` WHERE `bezeroNAN`=" . $_SESSION['uneko_NAN'] . "; ");
-    $ilara = mysqli_num_rows($elemKonprobaketa);
-    if($ilara < 5){
+    $unekoIlara = mysqli_fetch_array($elemKonprobaketa);
+    echo $unekoIlara['COUNT(*)'] ;
+    if($unekoIlara['COUNT(*)'] < 5){
         $query = mysqli_query($conn, "INSERT INTO `elementua`(`kirola`, `data`, `ordutegia`, `monitorea`, `gela`, `bezeroNAN`) VALUES ('$kirola','$data','$ordutegia','$monitorea','$gela','" . $_SESSION['uneko_NAN'] . "'); ");
         if(!$query){
           echo"Errore bat egon da. Errorea: " . $query . "<br>" . $conn->error;
         }
         else{
-            echo"datos guardado correctamente","<meta http-equiv='refresh' content='10; url=../logeatuta.html' />";
+            echo"<script>alert('Erreserba bete da.')</script>","<meta http-equiv='refresh' content='0; url=../logeatuta.html' />";
         }
     }
     else{
