0350d23efd0b98fa2c9a88922b0a7d2c1b293108
PeioLlano <peiollano@gmail.com>
2021-10-17 16:22:10 +0200

    Ezabatu elementua

---
+++ app/php/deleteElementua.php
@@ -0,0 +1,41 @@
+<?php
+  // phpinfo();
+  $hostname = "db";
+  $username = "admin";
+  $password = "test";
+  $db = "database";
+
+  $conn = mysqli_connect($hostname,$username,$password,$db);
+  if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+  }
+
+  session_start();
+
+  function getBetween($string, $start = "", $end = ""){
+    if (strpos($string, $start)) { // required if $start not exist in $string
+        $startCharCount = strpos($string, $start) + strlen($start);
+        $firstSubStr = substr($string, $startCharCount, strlen($string));
+        $endCharCount = strpos($firstSubStr, $end);
+        if ($endCharCount == 0) {
+            $endCharCount = strlen($firstSubStr);
+        }
+        return substr($firstSubStr, 0, $endCharCount);
+    } else {
+        return '';
+    }
+}
+
+  $erreserba = $_POST['erreserba'];
+
+  $kirola= strtok($erreserba, ',');
+  $data=getBetween($erreserba, ', Data:', ', Ordua:');
+  $ordutegia=getBetween($erreserba, ', Ordua:', '.');
+  
+  $query = mysqli_query($conn, "DELETE FROM `elementua` WHERE `kirola`='$kirola'  AND `data`='$data'  AND `ordutegia`='$ordutegia' ; ");
+  echo"<script language='javascript'>alert('Erreserba ezabatua izan da.');</script>";
+  echo"<meta http-equiv='refresh' content='0; url=klaseaKudeatu.php' />";
+
+  mysqli_free_result($query);
+  
+?>
---
+++ app/php/klaseaKudeatu.php
@@ -0,0 +1,109 @@
+<?php
+  // phpinfo();
+  $hostname = "db";
+  $username = "admin";
+  $password = "test";
+  $db = "database";
+
+  $conn = mysqli_connect($hostname,$username,$password,$db);
+  if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+  }
+
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="with=device-width, initial-scale=1.0">
+        <title>JP Polikiroldegia</title>
+        <link rel="icon" type="image/png" href="https://img.icons8.com/glyph-neue/64/000000/basketball.png" />	
+        <link rel="stylesheet" href="../css/styleKK.css">
+        <!--Letra mota aldatu ahal izateko-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300&display=swap" rel="stylesheet"> 
+        <!--Letra mota aldatu ahal izateko-->
+    </head>
+    <body>
+        <section class="header">
+            <nav>
+                <a href="index.html"><img src="../images/logo.png" ></a>
+                <div class="nav-links">
+                    <ul>
+                        <li><a href="klaseaKudeatu.php">Klaseak kudeatu</a></li>
+                        <li><a href="../klaseakIkusi.html">Klaseak ikusi</a></li>
+                        <li><a href="logeatuta.php">Nire Kontua</a></li>
+                        <li><a href="itxiSesioa.php">Itxi sesioa</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <div class="text-box">
+                <h1>Klaseak Sartu</h1>
+                <p> Hemen nahi dituzun erreserbak osatu ahalko dituzu.</p>
+            </div>
+        </section>
+        <section class="sartu">
+            <form class="formularioa" id="form" method="post" action="php/insertElementua.php">
+                <label class="label" for="kirola">Kirola hautatu:</label>
+
+                <select class="select" name="kirola" id="kirola">
+                  <option value="Areto Futbola">Areto futbola</option>
+                  <option value="Saskibaloia">Saskibaloia</option>
+                  <option value="Igeriketa">Igeriketa</option>
+                  <option value="Gimnasioa">Gimnasioa</option>
+                </select> 
+
+                <br>
+
+                <label class="label" for="data">Data:</label>
+
+                <input class="data" type="date" id="data" name="data"
+                    value="2018-07-22"
+                    min="2021-01-01" max="2050-12-31">
+
+                <br>
+
+                <label class="label" for="ordua">Ordua:</label>
+                <input class="ordua" type="time" id="ordutegia" name="ordutegia">
+                
+                <br>
+
+                <button type="reset" class="botoia">Ezabatu</button>
+                <button class="botoia">Erreserbatu </button>
+            </form>
+        </section>
+
+        <section class="ezabatu">
+            <div class="ezabatu_goiburu">
+                    <h1>Klaseak Kendu</h1>
+                    <p> Hemen nahi dituzun erreserbak kendu ahalko dituzu.</p>
+            </div>
+            <form class="formularioa" id="form" method="post" action="deleteElementua.php">
+                <label class="label" for="kirola">Kirola hautatu:</label>
+
+                <select class="select" name="erreserba" id="erreserba">
+            
+                    <?php
+                        $query = mysqli_query($conn, "SELECT * FROM `elementua` WHERE `bezeroNAN` = '" . $_SESSION['uneko_NAN'] . "' ; ");
+
+                        if (mysqli_num_rows($query) > 0) {
+                            while($ilara = mysqli_fetch_assoc($query)){
+                                echo "<option value='" . $ilara["kirola"] . ", Data:" . $ilara["data"] . ", Ordua:". $ilara["ordutegia"] . ".'>" . $ilara["kirola"] . " --> " . $ilara["data"] . "-ean ". $ilara["ordutegia"] . "-etan</option>";
+                            }   
+                        } else {
+                            echo "<option value="-">Ez duzu klase erreserbaturik.</option>";
+                        }
+
+                        mysqli_close($conn);
+                    ?>
+                </select> 
+                <br>
+
+                <button type="reset" class="botoia">Ezabatu</button>
+                <button class="botoia">Erreserba kendu </button>
+            </form>
+        </section>
+	</body>
+</html>
