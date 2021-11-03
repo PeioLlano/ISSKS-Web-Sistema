f489c719c3503e0a7ae0a15be9192a38fc7e4b11
PeioLlano <peiollano@gmail.com>
2021-11-03 18:50:06 +0100

    DatuBasea tarteekin

--- database.sql
+++ database.sql
@@ -64,12 +64,14 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
+/*--------------------------------------------------------------------------------------------------------------------------------*/
+
 -- phpMyAdmin SQL Dump
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 20-10-2021 a las 16:48:45
+-- Tiempo de generación: 03-11-2021 a las 17:48:54
 -- Versión del servidor: 10.6.4-MariaDB-1:10.6.4+maria~focal
 -- Versión de PHP: 7.4.20
 
@@ -96,7 +98,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `elementua` (
   `kirola` varchar(25) NOT NULL,
   `data` date NOT NULL,
-  `ordutegia` char(5) NOT NULL,
+  `ordutegia` char(11) NOT NULL,
   `monitorea` varchar(30) NOT NULL,
   `gela` varchar(15) NOT NULL,
   `bezeroNAN` char(9) NOT NULL
@@ -107,33 +109,35 @@ CREATE TABLE `elementua` (
 --
 
 INSERT INTO `elementua` (`kirola`, `data`, `ordutegia`, `monitorea`, `gela`, `bezeroNAN`) VALUES
-('Areto Futbola', '2021-11-13', '12:34', 'Jon', 'P7I5', '12345678Z'),
-('Areto Futbola', '2021-11-18', '16:06', 'Peio', 'P1I5', '12345678Z'),
-('Areto Futbola', '2021-11-21', '20:30', 'Aimar', 'P1I3', '12345678Z'),
-('Areto Futbola', '2021-12-17', '11:11', 'Peio', 'P1I2', '12345678Z'),
-('Areto Futbola', '2021-12-22', '11:30', 'Maider', 'P2I5', '47747474A'),
-('Gimnasia Erritmikoa', '2021-11-21', '11:45', 'Andoni', 'P3I3', '11223344B'),
-('Gimnasia Erritmikoa', '2021-12-22', '18:00', 'Peio', 'P1I2', '89987667Y'),
-('Gimnasia Erritmikoa', '2022-01-13', '09:00', 'Claudia', 'P1I2', '89987667Y'),
-('Gimnasioa', '2021-11-13', '11:11', 'Sara', 'P4I1', '11223344B'),
-('Gimnasioa', '2021-12-16', '10:00', 'Asier', 'P1I3', '89987667Y'),
-('Gimnasioa', '2021-12-17', '08:30', 'Peio', 'P7I5', '47747474A'),
-('Gimnasioa', '2022-01-06', '12:00', 'Ander', 'P1I3', '67543454K'),
-('Gimnasioa', '2022-01-07', '21:00', 'Peio', 'P1I5', '47747474A'),
-('Gimnasioa', '2022-01-14', '18:00', 'Irene', 'P1I3', '67543454K'),
-('Gimnasioa', '2022-11-01', '19:00', 'Irene', 'P1I3', '67543454K'),
-('Igeriketa', '2021-11-20', '18:00', 'Asier', 'P2I8', '87654321X'),
-('Igeriketa', '2021-12-16', '20:30', 'Claudia', 'P1I3', '11223344B'),
-('Igeriketa', '2021-12-17', '13:30', 'Peio', 'P2I4', '47747474A'),
-('Igeriketa', '2022-01-11', '10:00', 'Claudia', 'P2I8', '34456212G'),
-('Saskibaloia', '2021-11-22', '10:00', 'Markel', 'P1I2', '34456212G'),
-('Saskibaloia', '2021-12-08', '10:15', 'Andoni', 'P1I1', '67543454K'),
-('Saskibaloia', '2021-12-29', '19:00', 'Peio', 'P1I1', '87654321X'),
-('Saskibaloia', '2022-01-06', '20:02', 'Ainara', 'P2I7', '11223344B'),
-('Squash', '2021-12-17', '11:00', 'Miren', 'P1I5', '89987667Y'),
-('Squash', '2021-12-23', '13:30', 'Eneko', 'P1I3', '11223344B'),
-('Squash', '2021-12-24', '11:00', 'Peio', 'P1I3', '47747474A'),
-('Squash', '2022-01-26', '20:00', 'Asier', 'P1I5', '67543454K');
+('Areto Futbola', '2021-11-13', '10:00-11:00', 'Jon', 'P7I5', '12345678Z'),
+('Areto Futbola', '2021-11-18', '19:00-20:00', 'Peio', 'P1I5', '12345678Z'),
+('Areto Futbola', '2021-11-21', '16:00-17:00', 'Aimar', 'P1I3', '12345678Z'),
+('Areto Futbola', '2021-12-17', '10:00-11:00', 'Peio', 'P1I2', '12345678Z'),
+('Areto Futbola', '2021-12-22', '08:00-09:00', 'Maider', 'P2I5', '47747474A'),
+('Gimnasia Erritmikoa', '2021-11-21', '09:00-10:00', 'Andoni', 'P3I3', '11223344B'),
+('Gimnasia Erritmikoa', '2021-11-29', '19:00-20:00', 'Peio', 'P1I2', '87654321X'),
+('Gimnasia Erritmikoa', '2021-12-22', '20:00-21:00', 'Peio', 'P1I2', '89987667Y'),
+('Gimnasia Erritmikoa', '2022-01-13', '13:00-14:00', 'Claudia', 'P1I2', '89987667Y'),
+('Gimnasioa', '2021-11-13', '12:00-13:00', 'Sara', 'P4I1', '11223344B'),
+('Gimnasioa', '2021-11-26', '20:00-21:00', 'Markel', 'P7I5', '87654321X'),
+('Gimnasioa', '2021-12-16', '10:00-11:00', 'Asier', 'P1I3', '89987667Y'),
+('Gimnasioa', '2021-12-17', '08:00-09:00', 'Peio', 'P7I5', '47747474A'),
+('Gimnasioa', '2022-01-06', '10:00-11:00', 'Ander', 'P1I3', '67543454K'),
+('Gimnasioa', '2022-01-07', '20:00-21:00', 'Peio', 'P1I5', '47747474A'),
+('Gimnasioa', '2022-01-14', '20:00-21:00', 'Irene', 'P1I3', '67543454K'),
+('Gimnasioa', '2022-11-01', '20:00-21:00', 'Irene', 'P1I3', '67543454K'),
+('Igeriketa', '2021-12-16', '20:00-21:00', 'Claudia', 'P1I3', '11223344B'),
+('Igeriketa', '2021-12-17', '11:00-12:00', 'Peio', 'P2I4', '47747474A'),
+('Igeriketa', '2022-01-11', '10:00-11:00', 'Claudia', 'P2I8', '34456212G'),
+('Saskibaloia', '2021-11-22', '17:00-18:00', 'Markel', 'P1I2', '34456212G'),
+('Saskibaloia', '2021-12-08', '13:00-14:00', 'Andoni', 'P1I1', '67543454K'),
+('Saskibaloia', '2021-12-29', '18:00-19:00', 'Peio', 'P1I1', '87654321X'),
+('Saskibaloia', '2022-01-06', '19:00-20:00', 'Ainara', 'P2I7', '11223344B'),
+('Squash', '2021-12-17', '10:00-11:00', 'Miren', 'P1I5', '89987667Y'),
+('Squash', '2021-12-23', '13:00-14:00', 'Eneko', 'P1I3', '11223344B'),
+('Squash', '2021-12-24', '13:00-14:00', 'Peio', 'P1I3', '47747474A'),
+('Squash', '2022-01-26', '20:00-21:00', 'Asier', 'P1I5', '67543454K'),
+('Squash', '2022-11-09', '08:00-09:00', 'Markel', 'P1I5', '87654321X');
 
 --
 -- Índices para tablas volcadas
